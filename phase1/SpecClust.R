1a6cf891e214a12c9bef6fee52776035265dad17
yanbinc2 <95835790+yanbinc2@users.noreply.github.com>
2024-03-15 12:11:07 +0800

    Add files via upload

---
+++ phase1/SpecClust.R
@@ -0,0 +1,8 @@
+library(kknn)
+
+f1 <-read.csv("MNIST_tSNE_5000.csv")
+f2 <- read.csv("MNIST_Labels_5000.csv")
+s1 <- specClust(f1,20)
+f2$Spec20 <- s1$cluster
+
+write.csv(f2,"MNIST_Labels_Spec20.csv",row.names = FALSE)
