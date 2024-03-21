698b30d1fec273f7ecc324bb8e83bb4e3e44b82c
yanbinc2 <95835790+yanbinc2@users.noreply.github.com>
2024-03-21 19:57:36 +0800

    Add files via upload

--- phase1/codes/SpecClust.R
+++ phase1/codes/SpecClust.R
@@ -13,13 +13,5 @@ f2$Spec200 <- s1$cluster
 write.csv(f2,"ResNet18_PlantDisease_45K_tSNE_Spec.csv",row.names = FALSE)
 
 
-h1 <-read.csv("VGG16_PlantDisease_45K_Values.csv")
-h2 <- read.csv("VGG16_PlantDisease_45K_Labels.csv")
-s2 <- specClust(h1,200)
-h2$Spec200 <- s2$cluster
-
-
-write.csv(h2,"VGG16_PlantDisease_45K_tSNE_Spec.csv",row.names = FALSE)
-
 
 
